1799a7440b1fb00bad430899ef84f920d40bbc68
AgentDejavu <162957886+AgentDejavu@users.noreply.github.com>
2024-09-15 04:33:03 -0400

    Update settings.py

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'hpbv()ep00boce&o0w7z1h)st148(*m@6@-rk$nn)(n9ojj4c0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [resh-l2hu.onrender.com]
+ALLOWED_HOSTS = [35.160.120.126
+44.233.151.27
+34.211.200.85]
 
 
 # Application definition
